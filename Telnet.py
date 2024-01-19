7bde772ec94f297695ef156e86728ed93564e043
Julien Geyer <juliengeyer974@orange.fr>
2024-01-19 17:28:24 +0100

    Intent et Telnet

--- Telnet.py
+++ Telnet.py
@@ -1,13 +1,46 @@
 import telnetlib
 from telnetlib import Telnet
 
-HOST=["","","","","",""]
-AddIp=[]
+HOST=["R1","R2","R3","R4","R5","R6"]
+IpHost=[""] #adresse ip des routeurs pour communiquer avec eux (https://www.sysnettechsolutions.com/en/configure-telnet-gns3/ partie 8 et 9)
+area = 0
 for i in range (len(HOST)):
-    H=HOST[i]
+    H=IpHost[i]
     tn=telnetlib.Telnet(H)
     #chercher dans le json la plage et choisir une addresse ip via AddIp.append(Nouvelle addresse ip) et 
     #comparer avec les valeurs dans AddIp pour voir si elle a pas déjà été prise
+    tn.write(b"enable\n")
     tn.write(b"conf t\n")
-    #tn.write
-    #if valIGP json == RIP ....
+    tn.write(b"ipv6 unicast-routing\n")
+    #for allInterface
+    tn.write(b"interface"+ +"\n") #Mettre l'interface à paramétrer
+    tn.write(b"ipv6 enable\n")
+    tn.write(b"ipv6 address"+ +"\n")
+    tn.write(b"no shutdown\n")
+    tn.write(b"end\n")
+    
+    
+    #if RIP (pas dans le for allInsterface)
+    tn.write(b"conf t\n")
+    tn.write(b"ipv6 rip connected\n")
+    tn.write(b"redistribute connected\n")
+    tn.write(b"end\n")
+    tn.write(b"conf t\n")
+    #for allInterface2 (dans le if)
+    tn.write(b"ipv6 rip connected enable\n")
+    tn.write(b"end\n")
+
+    #if OSPF
+    tn.write(b"conf t\n")
+    processid = HOST[i][1:]
+    tn.write(b"ipv6 router ospf"+ processid +"\n")
+    routerid = processid+"."+processid+"."+processid+"."+processid
+    tn.write(b"router-id"+routerid+"\n")
+    tn.write(b"end\n")
+    tn.write(b"conf t\n")
+    #for allInterface3
+    tn.write(b"interface"+ +"\n")
+    tn.write(b"ipv6 ospf"+processid+"area"+area+"\n")
+    
+    area +=1 #hors de la bouble for allInterface3
+    
--- intent_to_config.py
+++ intent_to_config.py
@@ -1,82 +1,86 @@
 import json
-
-def generate_config(as_info):
+area = 0
+def generate_config(as_info,R):
     configs = []
 
     for as_name, as_data in as_info.items():
-        for router_name, router_data in as_data["routeurs"].items():
-            config = "version 15.2\nservice timestamps debug datetime msec\nservice timestamps log datetime msec\n!\n"
-            config += f"hostname {router}\n!\n"
-            config += "boot-start-marker\nboot-end-marker\n!\n"
-            config += "no aaa new-model\nno ip icmp rate-limit unreachable\nip cef\n!\n"
-            config += "no ip domain lookup\nipv6 unicast-routing\nipv6 cef\n!\n"
-            config +="multilink bundle-name authenticated\n!\n"
-            config += "ip tcp synwait-time 5\n!\n"
+        for router_name, router_data in as_data["routers"].items():
+            if router_name == R:
+                config = "version 15.2\nservice timestamps debug datetime msec\nservice timestamps log datetime msec\n!\n"
+                config += f"hostname {router_name}\n!\n"
+                config += "boot-start-marker\nboot-end-marker\n!\n"
+                config += "no aaa new-model\nno ip icmp rate-limit unreachable\nip cef\n!\n"
+                config += "no ip domain lookup\nipv6 unicast-routing\nipv6 cef\n!\n"
+                config +="multilink bundle-name authenticated\n!\n"
+                config += "ip tcp synwait-time 5\n!\n"
             
             # Générer les configurations d'interfaces
-            for subnetwork_number, interface_name in router_data.items():
-                config += f"interface {interface_name}\n"
-                config += f" ipv6 address {as_data['IP_range']['physical_interfaces']} link-local\n"
-                config += f" ipv6 address {as_data['IP_range']['physical_interfaces']}\n"
-                    # Ajouter d'autres configurations d'interface si nécessaire
-                    
-
-            # Générer les configurations spécifiques en fonction de l'IGP
-            if as_data["IGP"] == "RIP":
-                config += "router rip\n"
-                config += " version 2\n"
-                config += " no auto-summary\n"  # Supprimer la synthèse automatique
-                # Ajouter d'autres paramètres RIP si nécessaire
-
-            elif as_data["IGP"] == "OSPF":
-                config += "router ospf 1\n"
-                config += " router-id 1.1.1.1\n"  # Remplacer par un ID unique par routeur
-                # Ajouter d'autres paramètres OSPF si nécessaire
-
+                for subnetwork_number, interface_name in router_data.items():
+                    config += f"interface {interface_name}\n"
+                    config += " no ip address\n"
+                    if interface_name == "GigabitEthernet1/0" or "GigabitEthernet2/0" or "GigabitEthernet3/0" or "GigabitEthernet4/0":
+                        config += " negociation auto\n"
+                    elif interface_name == "FastEthernet0/0":
+                        config += " duplex full\n"
+                    config += f" ipv6 address {as_data['IP_range']['physical_interfaces']} link-local\n"
+                    config += f" ipv6 address {as_data['IP_range']['physical_interfaces']}\n"
+                    config += " ipv6 enable\n" 
+                        # Ajouter d'autres configurations d'interface si nécessaire
+                    if as_data["IGP"] == "RIP":
+                        config += " ipv6 rip ripng enable\n!\n"
+                        # Ajouter d'autres paramètres RIP si nécessaire
+                    elif as_data["IGP"] == "OSPF":
+                        config += f" ipv6 ospf 1 area 0\n!\n"
+                config += """interface Loopback0
+ no ip address
+ ipv6 address 2001:100::1/128
+ ipv6 enable
+!\n"""  
+        if R in list(as_data["neighbor"].keys()) :
+            config += """interface FastEthernet0/0
+ no ip address
+ duplex full
+ ipv6 address 2001:100:100:100::1/64
+ ipv6 enable
+!\n"""
             # Générer la configuration BGP commune pour tous les routeurs
-            if "eBGP" in as_info:
-                for link in as_info["eBGP"]["links"]:
-                    config += f"router bgp <AS-X>\n"
-                    config += f" neighbor {link[0]} remote-as <AS-Y>\n"
-                    config += f" neighbor {link[0]} update-source Loopback0\n"
-                    # Ajouter d'autres configurations eBGP si nécessaire
+        config += f"router bgp {as_name[2:]}\n"
+        number = R[1:]
+        config += f" bgp router-id {number}.{number}.{number}.{number}\n"
+        config += " bgp log-neighbor-changes\n"
+        config += " no bgp default ipv4-unicast"
+        #for
+        #config += f" neighbor {} remote-as <AS-Y>\n"
+        #if as_info.get("AS100", {}).get("neighbor", {})==
+        #config += f" neighbor {} update-source Loopback0\n"
 
-            # Générer la configuration spécifique pour le routeur
-            config += f"router bgp {as_data['BGP_AS']}\n"
-            config += f" bgp router-id 1.1.1.1\n"  # Remplacer par un ID unique par routeur
-            config += f" bgp log-neighbor-changes\n"
-            config += f" no bgp default ipv4-unicast\n"
+        # Générer les configurations de réseau pour BGP
+        config += " address-family ipv4\n"
+        config += " exit-address-family\n!\n"
+        config += " address-family ipv6\n"
 
-            for neighbor in as_data.get("BGP_neighbors", []):
-                config += f" neighbor {neighbor} remote-as {as_data['BGP_AS']}\n"
-                config += f" neighbor {neighbor} update-source Loopback0\n"
-                # Ajouter d'autres configurations BGP si nécessaire
-
-            # Générer les configurations de réseau pour BGP
-            config += " address-family ipv4\n"
-            config += " exit-address-family\n"
-            config += " address-family ipv6\n"
-            config += f"  network {as_data['IP_range']['physical_interfaces']}\n"
-            config += "  neighbor 2001:100::2 activate\n"
-            config += "  neighbor 2001:100::3 activate\n"
-            config += "  neighbor 2001:100:100:100::2 activate\n"
-            config += " exit-address-family\n"
-            config += "!\n"
-
-            configs.append(config)
-
+        config += " exit-address-family\n"
+        config += " !\n"
+        config += """ ip forward-protocol nd\n!\nno ip http server\nno ip http secure-server\n!
+ ipv6 router rip ripng\nredistribute connected\n!\ncontrol-plane\n!\nline con 0
+ exec-timeout 0 0\nprivilege level 15\nlogging synchronous\nstopbits 1\nline aux 0\nexec-timeout 0 0
+ privilege level 15\nlogging synchronous\nstopbits 1\nline vty 0 4\nlogin\n!\nend\n"""
+        configs.append(config)
     return configs
 
 def main():
-    with open("votre_fichier.json", "r") as file:
+    with open("intent.json", "r") as file:
         as_info = json.load(file)
-
-    router_configs = generate_config(as_info)
+    routeur = ["R1","R2","R3","R4","R5","R6"]
+    taille = len(routeur)
+    for i in range(taille):
+        router_configs = generate_config(as_info, routeur[i])
 
     # Écrire les configurations dans des fichiers séparés
-    for i, config in enumerate(router_configs):
-        with open(f"config_{as_info['AS1']['routeurs'][i]}.txt", "w") as file:
-            file.write(config)
+        for config, (as_name, as_data) in zip(router_configs, as_info.items()):
+            with open(f'config_{routeur[i]}.txt', "w") as file:
+                file.write(config)
+           
 
 if __name__ == "__main__":
     main()
